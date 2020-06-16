a92fe5f762145f2a03137c593fab3dd201953ab8
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-16 15:49:33 -0400

    form added

--- team-builder/src/form.js
+++ team-builder/src/form.js
@@ -1,3 +1,16 @@
 import React from "react";
 
-function Form() {}
+export default function Form() {
+  return (
+    <form>
+      <input type="text" name="name" placeholder="Enter Name"></input>
+      <input type="text" name="name" placeholder="Enter Email"></input>
+      <select>
+        <option>Trucker</option>
+        <option>School</option>
+        <option>Coder</option>
+      </select>
+      <input type="submit"></input>
+    </form>
+  );
+}
