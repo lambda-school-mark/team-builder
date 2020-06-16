2edbbf57cbd79ecc4157b88b9269661896a4964e
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-16 15:24:12 -0400

    added useState + memberList

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { memberList, setMemberList } = useState();
   return (
     <div className="App">
       <header className="App-header">
